e46a21e83011ee61ec47c6dd5901e809021e4ff6
James Canning <james@brudil.com>
2017-07-20 23:42:30 +0100

    Add Footer for The Drab

--- src/verticals/thedrab/components/Footer/index.js
+++ src/verticals/thedrab/components/Footer/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import logotypeWhite from '../../images/logotype.svg';
+import Logo from '../Logo';
 
 export default function Footer() {
   return (
     <footer className="Footer">
       <div className="Container">
-        <img
-          className="Footer__logotype"
-          src={logotypeWhite}
-          alt="The Drab | Content cares"
-          width="200"
-        />
-        <div className="Footer__tag">Some rights reserved.</div>
+        <div className="Footer__logo">
+          <Logo white />
+        </div>
+        <div className="Footer__tag">
+          is a satirical endeavor by{' '}
+          <a href="https://comsoc.co">
+            The Sussex Comedy Society
+          </a>.
+        </div>
       </div>
     </footer>
   );
